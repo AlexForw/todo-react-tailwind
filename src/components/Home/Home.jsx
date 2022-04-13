503ef6edf5bb43c36a25a630f33f35ce0fd7303c
AlexForw <lmityushkin@mail.ru>
2022-04-13 15:00:39 +0300

    feat: add few options

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -29,14 +29,22 @@ const Home = () =>{
         current.isCompleted = !current.isCompleted
         setTodos(copyArr)
     }
+    
+    const removeTodo = id => {
+        const newArr = todos.filter(elem => elem.id >= 0.5)
+        setTodos(newArr)
+        console.log(newArr)
+        console.log(todos)
+    }
 
     return(
         <div className="w-4/5 mx-auto text-white ">
             <h1 className="text-2xl text-center font-bold mb-10">Todo-react</h1>
             {
-                data.map((elem,i)=>{
+                todos.map((elem,i)=>{
+                    // console.log(elem)
                     return(
-                        <TodoItem checkComplete={checkComplete} key={i + elem} todo={elem}/>
+                        <TodoItem removeTodo={removeTodo} checkComplete={checkComplete} key={i + elem} todo={elem}/>
                     )
                 })
             }
--- src/components/items/CheckBox.jsx
+++ src/components/items/CheckBox.jsx
@@ -3,7 +3,7 @@ import {BsCheck} from 'react-icons/bs';
 
 const CheckBox = ({isCompleted}) => {
     return(
-        <div className='mr-2 border-2 rounded-lg border-pink-400 w-5 h-5'>
+        <div className={`mr-2 border-2 rounded-lg flex items-center justify-center border-pink-400 ${isCompleted ? 'bg-pink-400' : ''} w-7 h-7`}>
             { isCompleted &&
                 <BsCheck size={24} />
             }
