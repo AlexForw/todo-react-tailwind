99467e3a1b9ec5439680ae971cbbed10c4a46ad4
AlexForw <lmityushkin@mail.ru>
2022-05-26 01:52:46 +0300

    complete todo list

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,37 +3,39 @@ import TodoItem from "../items/TodoItem";
 
 
 
-const Home = () =>{
+const Home = () => {
 
-    const [value,setValue] = useState('')
+    const [value, setValue] = useState('')
 
 
     const [todos, setTodos] = useState([])
 
-    const checkComplete = (id) =>{
-        const copyArr = [...todos]
-        const current = copyArr.find(elem => elem.id === id)
-        current.isCompleted = !current.isCompleted
-        setTodos(copyArr)
-    }
-    
-    const removeTodo = todo => {
-        setTodos(todos.filter(elem => elem.id !== todo.id))
-        console.log(todos);
+    const checkComplete = (id) => {
+        const copy = [...todos]
+        const current = copy.find(elem => elem.id === id)
+        current.isComplete = !current.isComplete
+        setTodos(copy)
+        console.log(copy);
     }
 
-    return(
+    const removeTodo = (id) => {
+        setTodos([...todos].filter(elem => elem.id !== id))
+    }
+
+    return (
         <div className="w-4/5 mx-auto text-white ">
             <h1 className="text-2xl text-center font-bold mb-10">Todo-react</h1>
             <div>
-                <input type="text" value={value} className="text-black" onChange={(elem)=> setValue(elem.target.value)}/>
-                <button onClick={() => setTodos([...todos, value])}>Click</button>
+                <input type="text" value={value}
+                    className="text-black"
+                    onChange={(elem) => setValue(elem.target.value)} />
+
+                <button onClick={() => setTodos([...todos, { value, id: Math.random(), isComplete: false }])}>Click</button>
             </div>
             {
-                todos.map((elem,i)=>{
-                    // console.log(elem)
-                    return(
-                        <TodoItem removeTodo={removeTodo} checkComplete={checkComplete} key={i + elem} todo={elem}/>
+                todos.map((elem) => {
+                    return (
+                        <TodoItem removeTodo={removeTodo} checkComplete={checkComplete} key={elem.id} todo={elem} />
                     )
                 })
             }
--- src/components/items/TodoItem.jsx
+++ src/components/items/TodoItem.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import CheckBox from './CheckBox';
-import {BsTrash} from 'react-icons/bs'
+import { BsTrash } from 'react-icons/bs'
 
-const TodoItem = ({todo, checkComplete, removeTodo}) => { 
+const TodoItem = ({ todo, checkComplete, removeTodo }) => {
+    return (
+        <div onClick={() => checkComplete(todo.id)} className="mb-3 flex items-center rounded-2xl p-5 bg-gray-800 w-full">
+            <CheckBox isCompleted={todo.isComplete} />
+            <div className="mr-2 text-gray-500 hover:text-pink-800" onClick={(e) => {
+                e.stopPropagation()
+                removeTodo(todo.id)
+            }}><BsTrash size={24} /></div>
+            <div className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.value}</div>
 
-
-    return(
-        <button onClick={() => checkComplete(todo.id)} className="mb-3 flex items-center rounded-2xl p-5 bg-gray-800 w-full">
-            <CheckBox isCompleted={todo.isCompleted}/>
-            <div className="mr-2 text-gray-500 hover:text-pink-800" onClick={()=> removeTodo(todo)}><BsTrash size={24}/></div>
-                <div className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo}</div>
-            
-        </button>
+        </div>
     )
 }
 
